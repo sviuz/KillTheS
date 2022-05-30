6a148702c6a3b742511c342fd9fb5d1f953c616d
sviuz <kuibidanill@gmail.com>
2022-05-30 19:42:35 +0200

    fixes

--- Assets/Scripts/Level/Goal.cs
+++ Assets/Scripts/Level/Goal.cs
@@ -3,8 +3,7 @@
 namespace Level {
   public class Goal : Singleton<Goal> {
     public int _goal = 2;
-
-    private int _currentGoal;
+    public int _currentGoal;
 
     public void IncreaseCurrentGoal() {
       _currentGoal++;
--- Assets/Scripts/Ui/Level/LevelUI.cs
+++ Assets/Scripts/Ui/Level/LevelUI.cs
@@ -1,8 +1,17 @@
 ﻿using Core;
+using Level;
+using TMPro;
 using UnityEngine;
 
 namespace Ui.Level {
   public class LevelUI : Singleton<LevelUI> {
+    [SerializeField]
+    private TMP_Text _goal;
+
+    public void SetGoalText() {
+      _goal.text = $"{Goal.Instance._currentGoal.ToString()}/{Goal.Instance._goal.ToString()}";
+    }
+    
     public void Win() { }
 
     public void Lose() { }
